96b45a0f7f2e276bdcbfe4c46b1be5cb6bc56a83
Zorlac Realm <zorlac.realm@gmail.com>
2022-02-12 21:52:40 -0600

    Added swagger ui

--- src/main/java/xam/zorlac/servicebus/zorlacservicebus/ZorlacServiceBusApplication.java
+++ src/main/java/xam/zorlac/servicebus/zorlacservicebus/ZorlacServiceBusApplication.java
@@ -2,10 +2,9 @@ package xam.zorlac.servicebus.zorlacservicebus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
 
 @SpringBootApplication
-@ConfigurationPropertiesScan("xam.zorlac")
 public class ZorlacServiceBusApplication {
 
     public static void main(String[] args) {
